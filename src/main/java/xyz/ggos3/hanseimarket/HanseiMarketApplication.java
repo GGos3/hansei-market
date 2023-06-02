442da222b9cc213259e5b1db2b25e49f93e7807d
GGos3 <janpll0421@gmail.com>
2023-06-02 19:32:56 +0900

    chore: Spring init

---
+++ src/main/java/xyz/ggos3/hanseimarket/HanseiMarketApplication.java
@@ -0,0 +1,13 @@
+package xyz.ggos3.hanseimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanseiMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanseiMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ggos3/hanseimarket/HanseiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ggos3.hanseimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanseiMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
