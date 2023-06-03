e1153fbbbf6b872b78a26a66cdb9aad6d7ce9f80
GGos3 <janpll0421@gmail.com>
2023-06-03 16:04:24 +0900

    feat: vo 객체 추가

--- src/main/java/xyz/ggos3/hanseimarket/vo/user/UserCreateVO.java
+++ src/main/java/xyz/ggos3/hanseimarket/vo/user/UserCreateVO.java
@@ -1,14 +1,14 @@
-package xyz.ggos3.hanseimarket.dto.user.request;
+package xyz.ggos3.hanseimarket.vo.user;
 
 import lombok.Getter;
 
 @Getter
-public class UserCreateRequest {
+public class UserCreateVO {
     private final String name;
     private final String studentCode;
     private final String phoneNumber;
 
-    public UserCreateRequest(String name, String studentCode, String phoneNumber) {
+    public UserCreateVO(String name, String studentCode, String phoneNumber) {
         this.name = name;
         this.studentCode = studentCode;
         this.phoneNumber = phoneNumber;
