180c330c3468d555568b92ad34170777d15f6f1f
GGos3 <janpll0421@gmail.com>
2023-06-03 23:34:32 +0900

    feat: LoginId 검증 로직 추가

---
+++ src/main/java/xyz/ggos3/hanseimarket/service/user/UserService.java
@@ -0,0 +1,52 @@
+package xyz.ggos3.hanseimarket.service.user;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import xyz.ggos3.hanseimarket.domain.user.User;
+import xyz.ggos3.hanseimarket.domain.user.UserRepository;
+import xyz.ggos3.hanseimarket.domain.user.UserStatus;
+import xyz.ggos3.hanseimarket.domain.user.login.LoginUser;
+import xyz.ggos3.hanseimarket.domain.user.login.LoginUserRepository;
+import xyz.ggos3.hanseimarket.dto.user.request.UserCreateRequest;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final LoginUserRepository loginUserRepository;
+
+    @Transactional
+    public void createAccount(UserCreateRequest request) {
+        User newUser = new User(
+                request.getAccountId(),
+                request.getAccountPassword(),
+                request.getName(),
+                request.getStudentCode(),
+                request.getPhoneNumber()
+        );
+        LoginUser newLoginUser = new LoginUser(newUser);
+
+        validateLoginId(newUser);
+
+        userRepository.save(newUser);
+        loginUserRepository.save(newLoginUser);
+    }
+
+    @Transactional
+    public User findUserByAccountId(String accountId) {
+        return userRepository.findByUserId(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 id 입니다."));
+    }
+
+    private void validateLoginId(User user) {
+        userRepository.findByUserId(user.getUserId())
+                .filter(a -> a.getStatus().equals(UserStatus.enable))
+                .ifPresent(account -> {
+                    throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+                });
+    }
+}
