5b15f4ad463a6c67109c4cb86f8bb092ba2d84b4
GGos3 <janpll0421@gmail.com>
2023-06-03 23:28:41 +0900

    feat: 응답 객체 변경

--- src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
@@ -2,11 +2,13 @@ package xyz.ggos3.hanseimarket.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.ggos3.hanseimarket.dto.user.account.request.AccountCreateRequest;
-import xyz.ggos3.hanseimarket.service.UserService;
+import xyz.ggos3.hanseimarket.domain.user.User;
+import xyz.ggos3.hanseimarket.dto.user.request.UserCreateRequest;
+import xyz.ggos3.hanseimarket.service.user.UserService;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +17,10 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public HttpStatus createUser(@RequestBody AccountCreateRequest request) {
+    public ResponseEntity<User> createUser(@RequestBody UserCreateRequest request) {
         userService.createAccount(request);
 
-        return HttpStatus.CREATED;
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
