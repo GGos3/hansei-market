e4e9b04caa3daa84509f68f40d82bf1aa5fb1918
GGos3 <janpll0421@gmail.com>
2023-06-03 23:28:27 +0900

    feat: 로그인에 필요한 필드 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/user/User.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/user/User.java
@@ -9,11 +9,17 @@ import java.time.LocalDateTime;
 @Getter
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
+
+    private String userId;
+    private String userPassword;
+
     private String name;
     private String studentCode;
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status = UserStatus.enable;
 
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdDate;
@@ -21,21 +27,27 @@ public class User {
     public User() {
     }
 
-    public User(String name, String studentCode, String phoneNumber) {
+    public User(String userId, String userPassword, String name, String studentCode, String phoneNumber) {
+        this.userId = userId;
+        this.userPassword = userPassword;
         this.name = name;
-        this.studentCode = studentCode.toUpperCase();
+        this.studentCode = studentCode;
         this.phoneNumber = phoneNumber;
     }
 
-    public Class getStudentClass() {
+    public void disable() {
+        this.status = UserStatus.disable;
+    }
+
+    public SchoolClass getStudentClass() {
         char getClassCode = this.studentCode.charAt(0);
 
         return switch (getClassCode) {
-            case 'H' -> Class.해킹보안과;
-            case 'N' -> Class.네트워크보안과;
-            case 'G' -> Class.게임과;
-            case 'C' ->  Class.클라우드보안과;
-            case 'M' -> Class.메타버스게임과;
+            case 'H' -> SchoolClass.해킹보안과;
+            case 'N' -> SchoolClass.네트워크보안과;
+            case 'G' -> SchoolClass.게임과;
+            case 'C' ->  SchoolClass.클라우드보안과;
+            case 'M' -> SchoolClass.메타버스게임과;
             default -> throw new IllegalStateException("학과 정보를 찾을 수 없습니다. 학과코드: " + getClassCode);
         };
     }
