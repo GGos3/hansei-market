d4e9c1ec91339f37843cfc4c70326d128d13449c
GGos3 <janpll0421@gmail.com>
2023-06-03 23:27:32 +0900

    refactor: 코드 구조 수정

--- src/main/java/xyz/ggos3/hanseimarket/domain/user/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.ggos3.hanseimarket.domain.user.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByAccountId(String accountId);
-}
