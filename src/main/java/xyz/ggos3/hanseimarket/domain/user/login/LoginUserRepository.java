eb0ef533c1091d6e04dec043be1aa466d67096dd
GGos3 <janpll0421@gmail.com>
2023-06-04 23:38:37 +0900

    feat: findByLoginId 메서드 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/user/login/LoginUserRepository.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/user/login/LoginUserRepository.java
@@ -2,5 +2,8 @@ package xyz.ggos3.hanseimarket.domain.user.login;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoginUserRepository extends JpaRepository<LoginUser, Long> {
+    Optional<LoginUser> findByLoginId(String loginId);
 }
