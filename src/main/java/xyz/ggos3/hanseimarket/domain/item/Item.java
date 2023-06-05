80ebed40738985425acba6d3b74f1eb29a39a587
GGos3 <janpll0421@gmail.com>
2023-06-05 22:05:09 +0900

    feat: Getter, sumLike 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/item/Item.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/item/Item.java
@@ -1,11 +1,13 @@
 package xyz.ggos3.hanseimarket.domain.item;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import xyz.ggos3.hanseimarket.domain.user.User;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,7 +18,8 @@ public class Item {
 
     private String itemName;
     private String description;
-    private int like = 0;
+    private int likeCount = 0;
+    private int price = 0;
 
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdDate;
@@ -24,9 +27,14 @@ public class Item {
     public Item() {
     }
 
-    public Item(User user, String itemName, int like) {
+    public Item(User user, String itemName, int price, String description) {
         this.user = user;
         this.itemName = itemName;
-        this.like = like;
+        this.price = price;
+        this.description = description;
+    }
+
+    public void sumLike() {
+        this.likeCount =+ 1;
     }
 }
