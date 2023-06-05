f7ac4ddac31378862e615b64cd8b161bf2269f6e
GGos3 <janpll0421@gmail.com>
2023-06-05 22:05:20 +0900

    feat: findByUser 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/item/ItemRepository.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/item/ItemRepository.java
@@ -1,10 +1,13 @@
 package xyz.ggos3.hanseimarket.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.ggos3.hanseimarket.domain.user.User;
 
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(long id);
     Optional<Item> findByItemName(String itemName);
+
+    Optional<Item> findByUser(User user);
 }
