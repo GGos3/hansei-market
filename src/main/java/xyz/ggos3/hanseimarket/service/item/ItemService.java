da1d44e22a318ca208d573b212f7541f3b38abf7
GGos3 <janpll0421@gmail.com>
2023-06-05 22:06:02 +0900

    feat: saveItem, findItemByUserId 추가

--- src/main/java/xyz/ggos3/hanseimarket/service/item/ItemService.java
+++ src/main/java/xyz/ggos3/hanseimarket/service/item/ItemService.java
@@ -1,27 +1,50 @@
 package xyz.ggos3.hanseimarket.service.item;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.ggos3.hanseimarket.domain.item.Item;
 import xyz.ggos3.hanseimarket.domain.item.ItemRepository;
+import xyz.ggos3.hanseimarket.domain.user.User;
+import xyz.ggos3.hanseimarket.dto.item.request.ItemSaveRequest;
+import xyz.ggos3.hanseimarket.service.user.UserService;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final UserService userService;
 
-    public void saveItem() {
+    @Transactional
+    public void saveItem(ItemSaveRequest request) {
+        User requestUser = userService.findUserByUserId(request.getUserId());
+        Item newItem = new Item(
+                requestUser,
+                request.getItemName(),
+                request.getPrice(),
+                request.getDescription()
+        );
 
+        itemRepository.save(newItem);
     }
 
+    @Transactional
     public Item findItemByName(String itemName) {
         return itemRepository.findByItemName(itemName)
                 .orElseThrow(() -> new IllegalArgumentException("이름에 맞는 item이 없습니다."));
     }
 
+    @Transactional
     public Item findItemById(Long id) {
         return itemRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("id에 맞는 item이 없습니다."));
+    }
+
+    @Transactional
+    public Item findItemByUserId(String userId) {
+        User userByUserId = userService.findUserByUserId(userId);
+        return itemRepository.findByUser(userByUserId)
+                .orElseThrow(() -> new IllegalArgumentException("유저 Id에 맞는 item이 없습니다."));
     }
 }
