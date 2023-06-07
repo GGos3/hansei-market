89cf7c9afe8b64d5e3796eb1f5feb1cc629053d3
GGos3 <janpll0421@gmail.com>
2023-06-07 23:48:08 +0900

    chore: 필요없는 매서드 삭제

--- src/main/java/xyz/ggos3/hanseimarket/domain/item/ItemRepository.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/item/ItemRepository.java
@@ -6,7 +6,6 @@ import xyz.ggos3.hanseimarket.domain.user.User;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Optional<Item> findById(long id);
     Optional<Item> findByItemName(String itemName);
 
     Optional<Item> findByUser(User user);
