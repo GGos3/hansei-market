c15737bc74d2d9df85f3fb94a2642aa7f3a5f751
GGos3 <janpll0421@gmail.com>
2023-06-07 23:47:47 +0900

    feat: /item/save 매핑

--- src/main/java/xyz/ggos3/hanseimarket/controller/item/ItemController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/item/ItemController.java
@@ -1,7 +1,24 @@
 package xyz.ggos3.hanseimarket.controller.item;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import xyz.ggos3.hanseimarket.domain.item.Item;
+import xyz.ggos3.hanseimarket.dto.item.request.ItemSaveRequest;
+import xyz.ggos3.hanseimarket.service.item.ItemService;
 
 @RestController
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping("/item/save")
+    public void createItem(@RequestBody ItemSaveRequest request) {
+        itemService.saveItem(request);
+    }
+
+    @GetMapping("/item/{id}")
+    public Item getItem(@PathVariable Long id) {
+        return itemService.findItemById(id);
+    }
 }
