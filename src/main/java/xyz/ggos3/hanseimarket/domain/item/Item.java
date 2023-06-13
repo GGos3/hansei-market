1b33dfd9a97b93638804ffd0ff0121c750d73537
GGos3 <janpll0421@gmail.com>
2023-06-13 14:22:42 +0900

    feat: fetch type 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/item/Item.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/item/Item.java
@@ -13,7 +13,7 @@ public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
