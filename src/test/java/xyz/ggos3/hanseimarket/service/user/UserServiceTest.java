90cbd62102f3c3f2fe0a1e95d0d674d3cc808daa
GGos3 <janpll0421@gmail.com>
2023-06-13 14:40:42 +0900

    test: 유저 비활성화 테스트 추가

--- src/test/java/xyz/ggos3/hanseimarket/service/user/UserServiceTest.java
+++ src/test/java/xyz/ggos3/hanseimarket/service/user/UserServiceTest.java
@@ -1,11 +1,13 @@
 package xyz.ggos3.hanseimarket.service.user;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import xyz.ggos3.hanseimarket.domain.user.User;
 import xyz.ggos3.hanseimarket.domain.user.UserRepository;
+import xyz.ggos3.hanseimarket.domain.user.UserStatus;
 import xyz.ggos3.hanseimarket.domain.user.login.LoginUser;
 import xyz.ggos3.hanseimarket.domain.user.login.LoginUserRepository;
 import xyz.ggos3.hanseimarket.dto.user.request.UserCreateRequest;
@@ -26,10 +28,16 @@ class UserServiceTest {
         this.loginUserRepository = loginUserRepository;
     }
 
+    @AfterEach
+    void clear() {
+        loginUserRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("유저 생성")
     void createUserTest() {
-        UserCreateRequest request = new UserCreateRequest("test12", "testPass1234", "test", "H1111", "01011111111");
+        UserCreateRequest request = new UserCreateRequest("test123", "testPass1234", "test", "H1111", "01011111111");
 
         userService.createAccount(request);
 
@@ -40,5 +48,17 @@ class UserServiceTest {
         assertThat(user.getUserId()).isEqualTo(request.getUserId());
         // 로그인 전용 User의 user필드가가 user의 id와 같은지 검증
         assertThat(loginUser.getUser().getId()).isEqualTo(user.getId());
+    }
+
+
+    @Test
+    @DisplayName("유저 비활성화")
+    void disableUserTest() {
+        UserCreateRequest userCreateRequest = new UserCreateRequest("test123", "testPass1234", "test", "H1111", "010111111111");
+        userService.createAccount(userCreateRequest);
+
+        userService.disableUser("test123");
+
+        assertThat(userRepository.findByUserId("test123").get().getStatus()).isEqualTo(UserStatus.disable);
     }
 }
