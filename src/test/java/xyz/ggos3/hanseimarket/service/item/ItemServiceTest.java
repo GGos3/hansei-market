57d068d84be26c14e663aa8e6fdd0e37868517e6
GGos3 <janpll0421@gmail.com>
2023-06-14 12:15:13 +0900

    test: 테스트 순서 지정 및 updateItemTest 추가

--- src/test/java/xyz/ggos3/hanseimarket/service/item/ItemServiceTest.java
+++ src/test/java/xyz/ggos3/hanseimarket/service/item/ItemServiceTest.java
@@ -1,21 +1,20 @@
 package xyz.ggos3.hanseimarket.service.item;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import xyz.ggos3.hanseimarket.domain.item.ItemRepository;
 import xyz.ggos3.hanseimarket.domain.user.UserRepository;
 import xyz.ggos3.hanseimarket.domain.user.login.LoginUserRepository;
 import xyz.ggos3.hanseimarket.dto.item.request.ItemSaveRequest;
+import xyz.ggos3.hanseimarket.dto.item.request.ItemUpdateRequest;
 import xyz.ggos3.hanseimarket.dto.user.request.UserCreateRequest;
 import xyz.ggos3.hanseimarket.service.user.UserService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ItemServiceTest {
     private final ItemService itemService;
     private final UserService userService;
@@ -46,6 +45,7 @@ class ItemServiceTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("item 생성")
     void createItemTest() {
         ItemSaveRequest itemSaveRequest = new ItemSaveRequest("test123", "가방", 1000,"2023년도에 구매한 가방입니다");
@@ -58,14 +58,28 @@ class ItemServiceTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("조회수 카운트")
     void itemViewCountTest() {
         ItemSaveRequest itemSaveRequest = new ItemSaveRequest("test123", "가방", 1000,"2023년도에 구매한 가방입니다");
 
         itemService.saveItem(itemSaveRequest);
 
-        itemService.findItemById(1L);
+        itemService.findItemById(2L);
 
         assertThat(itemRepository.findAll().get(0).getView()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품 수정")
+    void updateItemTest() {
+        ItemSaveRequest itemSaveRequest = new ItemSaveRequest("test123", "가방", 1000,"2023년도에 구매한 가방입니다");
+        ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(3L, "모자", 1500, "멋진 모자!");
+
+        itemService.saveItem(itemSaveRequest);
+        itemService.updateItem(itemUpdateRequest);
+
+        assertThat(itemService.findItemById(3L).getItemName()).isEqualTo("모자");
     }
 }
