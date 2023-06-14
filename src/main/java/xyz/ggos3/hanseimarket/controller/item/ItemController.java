a3f64cc21e1033b7c322ef107de279c6ae193043
GGos3 <janpll0421@gmail.com>
2023-06-14 12:13:54 +0900

    feat: /item/all 매핑

--- src/main/java/xyz/ggos3/hanseimarket/controller/item/ItemController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/item/ItemController.java
@@ -8,6 +8,8 @@ import xyz.ggos3.hanseimarket.domain.item.Item;
 import xyz.ggos3.hanseimarket.dto.item.request.ItemSaveRequest;
 import xyz.ggos3.hanseimarket.service.item.ItemService;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ItemController {
@@ -24,5 +26,10 @@ public class ItemController {
     @GetMapping("/item/{id}")
     public Item getItem(@PathVariable Long id) {
         return itemService.findItemById(id);
+    }
+
+    @PostMapping("/item/all")
+    public List<Item> getAllItem() {
+        return itemService.findAll();
     }
 }
