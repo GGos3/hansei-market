354c3d751445f78a348fece2e48a3849f1d01b26
GGos3 <janpll0421@gmail.com>
2023-06-20 22:26:13 +0900

    feat: spring security filter 추가

---
+++ src/main/java/xyz/ggos3/hanseimarket/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package xyz.ggos3.hanseimarket.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/admin/**").hasRole("admin");
+                    req.requestMatchers("/", "/item/**", "/login/**").permitAll();
+                }).build();
+    }
+}
