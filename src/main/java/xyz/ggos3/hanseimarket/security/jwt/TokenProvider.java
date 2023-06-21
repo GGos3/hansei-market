fc39ce2983211c4527cf6314794fea9fb1702b63
GGos3 <janpll0421@gmail.com>
2023-06-21 23:43:20 +0900

    feat: jwt TokenProvider 추가

---
+++ src/main/java/xyz/ggos3/hanseimarket/security/jwt/TokenProvider.java
@@ -0,0 +1,51 @@
+package xyz.ggos3.hanseimarket.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Service
+@PropertySource("classpath:jwt.yml")
+public class TokenProvider {
+    private final String secretKey;
+    private final Long expirationHours;
+    private final String issuer;
+
+    public TokenProvider(
+            @Value("${secret-key}") String secretKey,
+            @Value("${expiration-hours}") Long expirationHours,
+            @Value("${issuer}") String issuer
+    ) {
+        this.secretKey = secretKey;
+        this.expirationHours = expirationHours;
+        this.issuer = issuer;
+    }
+
+    public String createToken(String userSpec) {
+        return Jwts.builder()
+                .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName()))
+                .setSubject(userSpec)
+                .setIssuer(issuer)
+                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
+                .setExpiration(Date.from(Instant.now().plus(expirationHours, ChronoUnit.HOURS)))
+                .compact();
+    }
+
+    public String validateTokenAndGetSubject(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
