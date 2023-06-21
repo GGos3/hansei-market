2c32990fdff94038c38b30e5538bf7c794499ece
GGos3 <janpll0421@gmail.com>
2023-06-21 23:53:07 +0900

    log: id 중복 확인시 id 로깅

--- src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
@@ -1,15 +1,18 @@
 package xyz.ggos3.hanseimarket.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.ggos3.hanseimarket.domain.user.User;
 import xyz.ggos3.hanseimarket.dto.user.request.UserCreateRequest;
+import xyz.ggos3.hanseimarket.dto.user.request.UserIdValidateRequest;
 import xyz.ggos3.hanseimarket.dto.user.response.UserInfoResponse;
 import xyz.ggos3.hanseimarket.service.user.UserService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -17,8 +20,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user/validate")
-    public ResponseEntity<?> validateUserId(@RequestBody String userId) {
-        userService.validateUser(userId);
+    public ResponseEntity<User> validateUserId(@RequestBody UserIdValidateRequest request) {
+        userService.validateUser(request.getUserId());
+        log.info("validate userId={}", request.getUserId());
 
         return ResponseEntity.ok().build();
     }
