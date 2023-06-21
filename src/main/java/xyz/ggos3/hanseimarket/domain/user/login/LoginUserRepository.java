7e6477f5e5a7ab5bc9e155961850ed4a40b79157
GGos3 <janpll0421@gmail.com>
2023-06-21 23:41:18 +0900

    chore: 메서드명 변경

--- src/main/java/xyz/ggos3/hanseimarket/domain/user/login/LoginUserRepository.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/user/login/LoginUserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LoginUserRepository extends JpaRepository<LoginUser, Long> {
-    Optional<LoginUser> findByLoginId(String loginId);
+    Optional<LoginUser> findByUserId(String loginId);
 }
 
