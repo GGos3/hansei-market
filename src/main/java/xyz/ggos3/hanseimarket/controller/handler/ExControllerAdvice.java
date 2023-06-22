eb74ef352f20c3af29e14cb8524c2737f1c6360f
GGos3 <janpll0421@gmail.com>
2023-06-22 14:01:44 +0900

    chore: 응답 객체 변경

--- src/main/java/xyz/ggos3/hanseimarket/controller/handler/ExControllerAdvice.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/handler/ExControllerAdvice.java
@@ -1,17 +1,17 @@
-package xyz.ggos3.hanseimarket.controller.advice;
+package xyz.ggos3.hanseimarket.controller.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import xyz.ggos3.hanseimarket.dto.ApiResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class ExControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentExHandler(IllegalArgumentException e) {
+    public ApiResponse illegalArgumentExHandler(IllegalArgumentException e) {
         log.error("[exceptionHandler] ex", e);
 
-        return ResponseEntity.status(409).body(e.getMessage());
+        return ApiResponse.error(e.getMessage());
     }
 }
