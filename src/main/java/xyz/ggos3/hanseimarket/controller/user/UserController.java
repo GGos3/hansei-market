f4065f2b2ae10eeaa29b2dde5aa48d8034eedaba
GGos3 <janpll0421@gmail.com>
2023-06-22 23:14:15 +0900

    feat: Swagger 설정 및 /user/liked-item 매핑

--- src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
@@ -1,5 +1,8 @@
 package xyz.ggos3.hanseimarket.controller.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.OnOpen;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,8 +10,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import xyz.ggos3.hanseimarket.dto.ApiResponse;
 import xyz.ggos3.hanseimarket.security.annotation.UserAuthorize;
+import xyz.ggos3.hanseimarket.service.item.like.LikedItemService;
 import xyz.ggos3.hanseimarket.service.user.UserService;
 
+@Tag(name = "회원관련 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +22,17 @@ import xyz.ggos3.hanseimarket.service.user.UserService;
 public class UserController {
 
     private final UserService userService;
-    @PostMapping
+    private final LikedItemService likedItemService;
+
+    @Operation(summary = "유저의 정보")
+    @PostMapping("/info")
     public ApiResponse showUserInfo(@AuthenticationPrincipal User user) {
         return ApiResponse.success(userService.findUserInfo(user.getUsername()));
+    }
+
+    @Operation(summary = "유저의 찜 리스트")
+    @PostMapping("/liked-item")
+    public ApiResponse showUserLikedItem(@AuthenticationPrincipal User user) {
+        return ApiResponse.success(likedItemService.findLikedItemsByUserId(user.getUsername()));
     }
 }
