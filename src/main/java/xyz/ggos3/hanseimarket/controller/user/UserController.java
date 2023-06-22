214c1c179be7d33408c9db63aa25bbfaeaa010ea
GGos3 <janpll0421@gmail.com>
2023-06-22 15:41:40 +0900

    feat: User정보 조회 기능 추가

--- src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/user/UserController.java
@@ -2,23 +2,23 @@ package xyz.ggos3.hanseimarket.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import xyz.ggos3.hanseimarket.dto.ApiResponse;
-import xyz.ggos3.hanseimarket.dto.user.request.UserInfoRequest;
 import xyz.ggos3.hanseimarket.security.annotation.UserAuthorize;
 import xyz.ggos3.hanseimarket.service.user.UserService;
 
 @Slf4j
-@UserAuthorize
 @RestController
 @RequiredArgsConstructor
+@UserAuthorize
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
     @PostMapping
-    public ApiResponse showUserInfo(@RequestBody UserInfoRequest request) {
-        return ApiResponse.success(userService.findUserInfo(request.userId()));
+    public ApiResponse showUserInfo(@AuthenticationPrincipal User user) {
+        return ApiResponse.success(userService.findUserInfo(user.getUsername()));
     }
 }
-
