ec3c16729ed5da45e1cffedd2471443a73246283
GGos3 <janpll0421@gmail.com>
2023-06-22 15:40:42 +0900

    feat: ExController 추가

--- src/main/java/xyz/ggos3/hanseimarket/controller/handler/ExControllerAdvice.java
+++ src/main/java/xyz/ggos3/hanseimarket/controller/handler/ExControllerAdvice.java
@@ -1,17 +1,47 @@
 package xyz.ggos3.hanseimarket.controller.handler;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import xyz.ggos3.hanseimarket.dto.ApiResponse;
 
+import java.security.SignatureException;
+import java.util.NoSuchElementException;
+
 @Slf4j
 @RestControllerAdvice
 public class ExControllerAdvice {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse illegalArgumentExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
+    @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
+    public ResponseEntity<ApiResponse> handleCommonException(Exception e) {
+        return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+    }
 
-        return ApiResponse.error(e.getMessage());
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponse> handleAccessDeniedException() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiResponse.error("접근이 거부되었습니다."));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleUnexpectedException() {
+        return ResponseEntity.internalServerError().body(ApiResponse.error("서버에 문제가 발생했습니다."));
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ApiResponse> handleSignatureException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("토큰이 유효하지 않습니다."));
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ApiResponse> handleMalformedJwtException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("올바르지 않은 토큰입니다."));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiResponse> handleExpiredJwtException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("토큰이 만료되었습니다. 다시 로그인해주세요."));
     }
 }
