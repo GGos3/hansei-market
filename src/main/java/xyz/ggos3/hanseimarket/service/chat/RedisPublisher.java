e3dd11674efa9029ac35c0b2ea68d1f22c3ae366
GGos3 <janpll0421@gmail.com>
2023-06-24 22:04:09 +0900

    feat: Redis pub, sub 추가

---
+++ src/main/java/xyz/ggos3/hanseimarket/service/chat/RedisPublisher.java
@@ -0,0 +1,18 @@
+package xyz.ggos3.hanseimarket.service.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+import xyz.ggos3.hanseimarket.domain.chat.ChatMessage;
+
+@Service
+@RequiredArgsConstructor
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
