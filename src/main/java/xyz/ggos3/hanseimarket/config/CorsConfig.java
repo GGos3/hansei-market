3baf2b09406da255e766362939d8461f569fda89
GGos3 <janpll0421@gmail.com>
2023-06-24 22:04:55 +0900

    chore: 클래스 패키지 변경

--- src/main/java/xyz/ggos3/hanseimarket/config/CorsConfig.java
+++ src/main/java/xyz/ggos3/hanseimarket/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package xyz.ggos3.hanseimarket.security;
+package xyz.ggos3.hanseimarket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
