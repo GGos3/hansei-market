1271e1eff710f426d102704ad117ff67cf3316f7
GGos3 <janpll0421@gmail.com>
2023-06-29 23:25:35 +0900

    feat: 게시글 엔티티 추가

---
+++ src/main/java/xyz/ggos3/hanseimarket/domain/post/Post.java
@@ -0,0 +1,40 @@
+package xyz.ggos3.hanseimarket.domain.post;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import xyz.ggos3.hanseimarket.domain.user.User;
+import xyz.ggos3.hanseimarket.domain.user.auth.AuthUser;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30)
+    private String postName;
+    @Column(length = 500)
+    private String postBody;
+    private int view = 0;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private AuthUser authUser;
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    @Builder
+    public Post(String postName, String postBody, int view, AuthUser user) {
+        this.postName = postName;
+        this.postBody = postBody;
+        this.view = view;
+        this.authUser = user;
+    }
+
+    public void increaseView() {
+        this.view++;
+    }
+}
