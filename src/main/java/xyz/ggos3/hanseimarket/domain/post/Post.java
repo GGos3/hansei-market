70c79b6816a404c9d42b18f74e390c8c40244768
GGos3 <janpll0421@gmail.com>
2023-06-30 23:37:31 +0900

    chore: Getter 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/post/Post.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/post/Post.java
@@ -3,26 +3,32 @@ package xyz.ggos3.hanseimarket.domain.post;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import xyz.ggos3.hanseimarket.domain.user.User;
 import xyz.ggos3.hanseimarket.domain.user.auth.AuthUser;
 
 import java.sql.Timestamp;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(length = 30)
     private String postName;
+
     @Column(length = 500)
     private String postBody;
+
     private int view = 0;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private AuthUser authUser;
+
     @CreationTimestamp
     private Timestamp createDate;
 
@@ -30,7 +36,6 @@ public class Post {
     public Post(String postName, String postBody, int view, AuthUser user) {
         this.postName = postName;
         this.postBody = postBody;
-        this.view = view;
         this.authUser = user;
     }
 
