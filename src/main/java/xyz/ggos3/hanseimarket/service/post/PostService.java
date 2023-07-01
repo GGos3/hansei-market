00cf4c90678bbe6937f4e3235ac61cb8853ef2b4
GGos3 <janpll0421@gmail.com>
2023-07-01 23:42:30 +0900

    feat: 느슨한 결합 구현

--- src/main/java/xyz/ggos3/hanseimarket/service/post/PostService.java
+++ src/main/java/xyz/ggos3/hanseimarket/service/post/PostService.java
@@ -1,92 +1,22 @@
 package xyz.ggos3.hanseimarket.service.post;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.ggos3.hanseimarket.domain.post.Post;
-import xyz.ggos3.hanseimarket.domain.post.PostRepository;
-import xyz.ggos3.hanseimarket.domain.post.comment.Comment;
-import xyz.ggos3.hanseimarket.domain.user.auth.AuthUser;
-import xyz.ggos3.hanseimarket.dto.post.comment.response.CommentResponse;
 import xyz.ggos3.hanseimarket.dto.post.request.PostBodyUpdateRequest;
 import xyz.ggos3.hanseimarket.dto.post.request.PostNameUpdateRequest;
 import xyz.ggos3.hanseimarket.dto.post.request.PostSaveRequest;
-import xyz.ggos3.hanseimarket.dto.post.response.PostResponse;
-import xyz.ggos3.hanseimarket.service.post.comment.CommentService;
-import xyz.ggos3.hanseimarket.service.user.auth.AuthUserService;
-
-import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-public class PostService {
+public interface PostService {
+    Post findPostById(Long id);
 
-    private final PostRepository postRepository;
-    private final AuthUserService authUserService;
-    private final CommentService commentService;
+    Post savePost(String uuid, PostSaveRequest request);
 
-    @Transactional
-    public PostResponse getPost(Long id) {
-        Post post = findPostById(id);
-        List<Comment> comments = commentService.findCommentByPost(post.getId());
-        post.increaseView();
+    void updatePostName(String uuid, PostNameUpdateRequest request);
 
-        return PostResponse.builder()
-                .postId(post.getId())
-                .postName(post.getPostName())
-                .postBody(post.getPostBody())
-                .postComments(
-                        comments.stream().map(comment ->
-                                CommentResponse.builder()
-                                        .userId(comment.getAuthUser().getUserId())
-                                        .comment(comment.getComment())
-                                        .createDate(comment.getCreateDate()).build()
-                        ).toList()
-                ).build();
-    }
+    void updatePostBody(String uuid, PostBodyUpdateRequest request);
 
-    @Transactional
-    public Post findPostById(Long id) {
-        return postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물 입니다."));
-    }
+    void deletePost(String uuid, Long id);
 
-    @Transactional
-    public Post savePost(String userId, PostSaveRequest request) {
-        AuthUser user = authUserService.findByUuid(userId);
-        Post post = Post.builder()
-                .user(user)
-                .postName(request.postName())
-                .postBody(request.postBody())
-                .build();
-
-        return postRepository.save(post);
-    }
-
-    @Transactional
-    public void updatePostName(String userId, PostNameUpdateRequest request) {
-        Post post = isOwner(userId, request.postId());
-        postRepository.updatePostName(post.getId(), request.postName());
-    }
-
-    @Transactional
-    public void updatePostBody(String userId, PostBodyUpdateRequest request) {
-        Post post = isOwner(userId, request.postId());
-        postRepository.updatePostBody(post.getId(), request.postBody());
-    }
-
-    @Transactional
-    public void deletePost(String userId, Long postId) {
-        Post post = isOwner(userId, postId);
-        postRepository.delete(post);
-    }
-
-    private Post isOwner(String userId, Long postId) {
-        authUserService.findByUuid(userId);
-        Post post = findPostById(postId);
-
-        if (!post.getAuthUser().getUserId().equals(userId))
-            throw new IllegalArgumentException("게시글 수정, 삭제는 본인만 할 수 있습니다");
-        return post;
-    }
+    void clearAll();
 }
--- src/main/java/xyz/ggos3/hanseimarket/service/post/comment/CommentService.java
+++ src/main/java/xyz/ggos3/hanseimarket/service/post/comment/CommentService.java
@@ -1,86 +1,24 @@
 package xyz.ggos3.hanseimarket.service.post.comment;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.ggos3.hanseimarket.domain.post.Post;
 import xyz.ggos3.hanseimarket.domain.post.comment.Comment;
-import xyz.ggos3.hanseimarket.domain.post.comment.CommentRepository;
-import xyz.ggos3.hanseimarket.domain.user.auth.AuthUser;
 import xyz.ggos3.hanseimarket.dto.post.comment.request.CommentSaveRequest;
 import xyz.ggos3.hanseimarket.dto.post.comment.request.CommentUpdateRequest;
 import xyz.ggos3.hanseimarket.dto.post.comment.response.CommentResponse;
-import xyz.ggos3.hanseimarket.service.post.PostService;
-import xyz.ggos3.hanseimarket.service.user.auth.AuthUserService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
-public class CommentService {
+public interface CommentService {
+    CommentResponse getComment(Long id);
 
-    private final AuthUserService authUserService;
-    private final CommentRepository commentRepository;
-    private final PostService postService;
+    Comment findCommentById(Long id);
+    List<Comment> findCommentByPost(Post post);
 
-    @Transactional
-    public CommentResponse getComment(Long id) {
-        Comment comment = findCommentById(id);
-        return new CommentResponse(
-                comment.getAuthUser().getUserId(),
-                comment.getComment(),
-                comment.getCreateDate()
-        );
-    }
+    Comment saveComment(String uuid, CommentSaveRequest request);
 
-    @Transactional
-    public Comment findCommentById(Long id) {
-        return commentRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글 입니다."));
-    }
+    Comment updateComment(String uuid, CommentUpdateRequest request);
 
-    @Transactional
-    public List<Comment> findCommentByPost(Long postId) {
-        return Optional.of(
-                commentRepository.findByPost(
-                        postService.findPostById(postId)))
-                .orElseThrow(() -> new IllegalArgumentException("댓글이 존재하지 않습니다."));
-    }
-
-    @Transactional
-    public Comment saveComment(String userId, CommentSaveRequest request) {
-        AuthUser user = authUserService.findByUuid(userId);
-        Post post = postService.findPostById(request.postId());
-
-        Comment comment = new Comment(request.comment(), user, post);
-
-        return commentRepository.save(comment);
-    }
-
-    @Transactional
-    public Comment updateComment(String userId, CommentUpdateRequest request) {
-        isOwner(userId, request.commentId());
-
-        commentRepository.updateComment(request.commentId(), request.comment());
-
-        return findCommentById(request.commentId());
-    }
-
-    @Transactional
-    public void deleteComment(String userId, Long id) {
-        Comment comment = isOwner(userId, id);
-
-        commentRepository.delete(comment);
-    }
-
-    private Comment isOwner(String userId, Long id) {
-        AuthUser user = authUserService.findByUuid(userId);
-        Comment comment = findCommentById(id);
-
-        if (!comment.getAuthUser().getUserId().equals(user.getUserId()))
-            throw new IllegalArgumentException("댓글은 본인만 수정, 삭제할 수 있습니다.");
-
-        return comment;
-    }
+    void deleteComment(String uuid, Long id);
 }
