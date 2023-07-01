8c534bdf463a39c77146c96a0e998ee36c1bf603
GGos3 <janpll0421@gmail.com>
2023-07-01 16:43:37 +0900

    feat: 게시글 & 댓글 조회기능 추가

--- src/main/java/xyz/ggos3/hanseimarket/service/post/PostService.java
+++ src/main/java/xyz/ggos3/hanseimarket/service/post/PostService.java
@@ -5,10 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.ggos3.hanseimarket.domain.post.Post;
 import xyz.ggos3.hanseimarket.domain.post.PostRepository;
+import xyz.ggos3.hanseimarket.domain.post.comment.Comment;
 import xyz.ggos3.hanseimarket.domain.user.auth.AuthUser;
+import xyz.ggos3.hanseimarket.dto.post.comment.response.CommentResponse;
+import xyz.ggos3.hanseimarket.dto.post.request.PostBodyUpdateRequest;
+import xyz.ggos3.hanseimarket.dto.post.request.PostNameUpdateRequest;
 import xyz.ggos3.hanseimarket.dto.post.request.PostSaveRequest;
 import xyz.ggos3.hanseimarket.dto.post.response.PostResponse;
+import xyz.ggos3.hanseimarket.service.post.comment.CommentService;
 import xyz.ggos3.hanseimarket.service.user.auth.AuthUserService;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +23,26 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final AuthUserService authUserService;
+    private final CommentService commentService;
 
+    @Transactional
     public PostResponse getPost(Long id) {
-        return new PostResponse(findPostById(id));
+        Post post = findPostById(id);
+        List<Comment> comments = commentService.findCommentByPost(post.getId());
+        post.increaseView();
+
+        return PostResponse.builder()
+                .postId(post.getId())
+                .postName(post.getPostName())
+                .postBody(post.getPostBody())
+                .postComments(
+                        comments.stream().map(comment ->
+                                CommentResponse.builder()
+                                        .userId(comment.getAuthUser().getUserId())
+                                        .comment(comment.getComment())
+                                        .createDate(comment.getCreateDate()).build()
+                        ).toList()
+                ).build();
     }
 
     @Transactional
@@ -37,5 +61,32 @@ public class PostService {
                 .build();
 
         return postRepository.save(post);
+    }
+
+    @Transactional
+    public void updatePostName(String userId, PostNameUpdateRequest request) {
+        Post post = isOwner(userId, request.postId());
+        postRepository.updatePostName(post.getId(), request.postName());
+    }
+
+    @Transactional
+    public void updatePostBody(String userId, PostBodyUpdateRequest request) {
+        Post post = isOwner(userId, request.postId());
+        postRepository.updatePostBody(post.getId(), request.postBody());
+    }
+
+    @Transactional
+    public void deletePost(String userId, Long postId) {
+        Post post = isOwner(userId, postId);
+        postRepository.delete(post);
+    }
+
+    private Post isOwner(String userId, Long postId) {
+        authUserService.findByUuid(userId);
+        Post post = findPostById(postId);
+
+        if (!post.getAuthUser().getUserId().equals(userId))
+            throw new IllegalArgumentException("게시글 수정, 삭제는 본인만 할 수 있습니다");
+        return post;
     }
 }
