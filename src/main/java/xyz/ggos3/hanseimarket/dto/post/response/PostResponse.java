a2df2212218b5f993530833c59a2107cd254a18d
GGos3 <janpll0421@gmail.com>
2023-07-01 16:43:04 +0900

    feat: PostResponse dto 추가

--- src/main/java/xyz/ggos3/hanseimarket/dto/post/response/PostResponse.java
+++ src/main/java/xyz/ggos3/hanseimarket/dto/post/response/PostResponse.java
@@ -1,24 +1,18 @@
 package xyz.ggos3.hanseimarket.dto.post.response;
 
 import lombok.Builder;
-import xyz.ggos3.hanseimarket.domain.post.Post;
+import xyz.ggos3.hanseimarket.dto.post.comment.response.CommentResponse;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+@Builder
 public record PostResponse(
         Long postId,
         String userName,
         String postName,
         String postBody,
+        List<CommentResponse> postComments,
         Timestamp createDate
 ) {
-    public PostResponse(Post post) {
-        this(
-                post.getId(),
-                post.getAuthUser().getUserId(),
-                post.getPostName(),
-                post.getPostBody(),
-                post.getCreateDate()
-        );
-    }
 }
