ef74edc79ba9302dcd6e7b9a57de02b53eda2a3e
GGos3 <janpll0421@gmail.com>
2023-07-06 10:33:46 +0900

    feat: 생성자 추가

--- src/main/java/xyz/ggos3/hanseimarket/domain/post/comment/dto/response/CommentResponse.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/post/comment/dto/response/CommentResponse.java
@@ -1,13 +1,20 @@
 package xyz.ggos3.hanseimarket.domain.post.comment.dto.response;
 
 import lombok.Builder;
+import xyz.ggos3.hanseimarket.domain.post.comment.Comment;
 
 import java.sql.Timestamp;
 
-@Builder
 public record CommentResponse(
         String userId,
         String comment,
         Timestamp createDate
 ) {
+    public CommentResponse(Comment comment) {
+        this(
+                comment.getAuthUser().getUserId(),
+                comment.getComment(),
+                comment.getCreateDate()
+        );
+    }
 }
