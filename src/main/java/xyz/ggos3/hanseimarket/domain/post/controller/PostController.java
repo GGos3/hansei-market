656d07a1e7705ec68e42c4dba21745aec5e1dcaf
GGos3 <janpll0421@gmail.com>
2023-07-06 11:47:21 +0900

    feat: "/post/new" 매핑

--- src/main/java/xyz/ggos3/hanseimarket/domain/post/controller/PostController.java
+++ src/main/java/xyz/ggos3/hanseimarket/domain/post/controller/PostController.java
@@ -2,15 +2,17 @@ package xyz.ggos3.hanseimarket.domain.post.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import xyz.ggos3.hanseimarket.common.dto.ApiResponse;
+import xyz.ggos3.hanseimarket.common.security.annotation.UserAuthorize;
 import xyz.ggos3.hanseimarket.domain.post.comment.dto.response.CommentResponse;
 import xyz.ggos3.hanseimarket.domain.post.comment.service.CommentService;
 import xyz.ggos3.hanseimarket.domain.post.domain.Post;
+import xyz.ggos3.hanseimarket.domain.post.dto.request.PostSaveRequest;
 import xyz.ggos3.hanseimarket.domain.post.dto.response.PostResponse;
 import xyz.ggos3.hanseimarket.domain.post.service.PostService;
+import xyz.ggos3.hanseimarket.domain.user.domain.User;
 
 import java.util.List;
 
@@ -36,5 +38,11 @@ public class PostController {
                         .createDate(post.getCreateDate())
                         .build()
         );
+    }
+
+    @UserAuthorize
+    @PostMapping("/post/new")
+    public ApiResponse<String> createPost(@AuthenticationPrincipal User user, @RequestBody PostSaveRequest request) {
+        return ApiResponse.success("게시글이 저장되었습니다.", request.postName());
     }
 }
