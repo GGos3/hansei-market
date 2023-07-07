c3e4f81f0f2d56a231fc64914c839be5b28c8748
GGos3 <janpll0421@gmail.com>
2023-07-07 11:19:05 +0900

    feat: 로그 추가

--- src/main/java/xyz/ggos3/hanseimarket/common/security/jwt/TokenProvider.java
+++ src/main/java/xyz/ggos3/hanseimarket/common/security/jwt/TokenProvider.java
@@ -2,6 +2,7 @@ package xyz.ggos3.hanseimarket.common.security.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,16 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+@Slf4j
 @Service
-@PropertySource("classpath:jwt.yml")
+@PropertySource("classpath:application.yml")
 public class TokenProvider {
     private final String secretKey;
     private final Long expirationHours;
     private final String issuer;
 
     public TokenProvider(
-            @Value("${secret-key}") String secretKey,
+            @Value("${secret-key") String secretKey,
             @Value("${expiration-hours}") Long expirationHours,
             @Value("${issuer}") String issuer
     ) {
@@ -31,6 +33,7 @@ public class TokenProvider {
     }
 
     public String createToken(String userSpec) {
+        log.info("SECRET-KEY={}", secretKey);
         return Jwts.builder()
                 .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName()))
                 .setSubject(userSpec)
